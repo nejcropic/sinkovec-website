dd1fd6b99b2b8c4735797e6c9703566f5d291d94
nejcropic <nejc.ropic@gmail.com>
2024-07-13 22:08:06 +0200

    .json

--- src/index.js
+++ src/index.js
@@ -5,49 +5,16 @@ import { BrowserRouter } from "react-router-dom";
 
 import Logo from "./images/logo_transparent.png";
 import LogoBlue from "./images/logo_transparent_blue.png";
-import Okolica from "./icons/plain.png";
-import Rusenje from "./icons/building.png";
-import Drobljenje from "./icons/crushing.png";
-import Dostava from "./icons/delivery-truck.png";
-import Izkopi from "./icons/excavator.png";
-import Dvorisce from "./icons/backyard.png";
-import Skarpa from "./icons/stones.png";
-import Traktor from "./icons/tractors.png";
-import Zid from "./icons/wall.png";
 
 const PreloadImages = () => {
   useEffect(() => {
-    const imagesToPreload = [
-      Logo,
-      LogoBlue,
-      Okolica,
-      Rusenje,
-      Drobljenje,
-      Dostava,
-      Izkopi,
-      Dvorisce,
-      Skarpa,
-      Traktor,
-      Zid,
-    ];
+    const imagesToPreload = [Logo, LogoBlue];
 
     imagesToPreload.forEach((imageSrc) => (new Image().src = imageSrc));
   }, []);
 };
 
-export {
-  Logo,
-  LogoBlue,
-  Okolica,
-  Rusenje,
-  Drobljenje,
-  Dostava,
-  Izkopi,
-  Dvorisce,
-  Skarpa,
-  Traktor,
-  Zid,
-};
+export { Logo, LogoBlue };
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
